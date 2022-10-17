bc9bbab2ead5683f6eba4f9b7cbdb5e2e51097ee
frontWeb-dev <frontend_web@kakao.com>
2022-10-17 19:27:22 +0900

    feat : Header 컴포넌트 생성

---
+++ client/src/components/Header/Header.styles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 4rem;
+  padding: 0 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Inner = styled.div``;
+
+export { Wrapper, Inner };
