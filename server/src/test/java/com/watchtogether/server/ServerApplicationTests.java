c16a4f43599cfe6784ae8bcb32ef9a48edb60e7b
kdjdj106 <kdjdj106@naver.com>
2022-10-17 15:44:49 +0900

    test : pr 테스트를 위한 커밋2

--- server/src/test/java/com/watchtogether/server/ServerApplicationTests.java
+++ server/src/test/java/com/watchtogether/server/ServerApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServerApplicationTests {
-    //test 주석입니다
+    //test 주석입니다2
     @Test
     void contextLoads() {
     }
