16fb6f8c738b6e9aa2958ed98df4fc35e2566060
frontWeb-dev <frontend_web@kakao.com>
2022-10-17 15:43:25 +0900

    style: 전역 스타일 추가
    - theme : 색상 변수 파일 추가
    - 전역 스타일 파일 추가

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// styles
+import './styles/reset.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme/theme';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ client/src/styles/Input.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 100%;
+  height: 50px;
+  padding: 0 16px;
+  margin-bottom: 16px;
+  outline: none;
+  border: 1px solid ${(props) => props.theme.border};
+  border-radius: 12px;
+`;
+
+export default { Input };
