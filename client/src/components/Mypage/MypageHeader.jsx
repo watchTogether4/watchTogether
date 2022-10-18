39e2cf88a837a931d4d1a74e4bb5058cf85a8bd6
frontWeb-dev <frontend_web@kakao.com>
2022-10-18 18:54:24 +0900

    feat: 마이페이지 헤더 구현

---
+++ client/src/components/Mypage/MypageHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper, SubTitle } from './MypageHeader.styles';
+
+function MypageHeader() {
+  return (
+    <Wrapper>
+      <SubTitle>마이 페이지</SubTitle>
+    </Wrapper>
+  );
+}
+
+export default MypageHeader;
