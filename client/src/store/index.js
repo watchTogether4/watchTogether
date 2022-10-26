eb6495cc94fae07af9e26dcd62609c3ad6362792
frontWeb-dev <frontend_web@kakao.com>
2022-10-26 17:44:44 +0900

    chore : 폴더 이름 변경
    redux -> store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user';
+import userReducer from './User';
+import tokenReducer from './Auth';
 
 export default configureStore({
   reducer: {
     user: userReducer,
-
+    authToken: tokenReducer,
   },
 });
