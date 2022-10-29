d22f224ad710c0f9e412663482e38056fff5f318
frontWeb-dev <frontend_web@kakao.com>
2022-10-30 05:17:38 +0900

    fix: Api 수정 및 추가
    - BASE_URL로 겹치는 부분 분리
    - 닉네임 검색 api 추가

--- client/src/api/Users.js
+++ client/src/api/Users.js
@@ -4,9 +4,12 @@ import axios from 'axios';
  * 로그인 - POST
  * @param { {email: string, password: string} } data
  */
+
+const BASE_URL = '/api/v1/users';
+
 export const loginUser = async (data) => (
   axios({
-    url: '/api/v1/users/sign-in',
+    url: `${BASE_URL}/sign-in`,
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     data: JSON.stringify(data),
@@ -17,7 +20,7 @@ export const loginUser = async (data) => (
  * 로그아웃 - GET
  */
 export const logoutUser = () => axios({
-  url: '/api/v1/users/sign-in',
+  url: `${BASE_URL}/sign-in`,
   method: 'GET',
   headers: { 'Content-Type': 'application/json' },
 });
@@ -27,7 +30,21 @@ export const logoutUser = () => axios({
  * 토큰 값 만료 확인 여부 
  */
 export const isValidateToken = () => axios({
-  url: '/api/v1/users/my-page',
+  url: `${BASE_URL}/my-page`,
   method: 'GET',
   headers: { 'Content-Type': 'application/json' },
 });
+
+
+/**
+ * 파티원 초대 - 닉네임 검색 
+ * @param {string} name 
+ */
+export const searchUser = (name) => axios({
+  url: `${BASE_URL}/search-user`,
+  method: 'GET',
+  headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+  params: {
+    nickname: name
+  },
+})
