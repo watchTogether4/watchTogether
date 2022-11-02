17c6be2137d6afef396a6f757f447fc55fa95ff8
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2022-11-02 18:10:51 +0900

    feat: 라우터 및 framer 라이브러리 추가
    feat: 라우터 및 framer 라이브러리 추가

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,19 @@ import AddParty from './pages/AddPartyPage';
 import UserInfo from './pages/UserInfoPage';
 import PartyList from './pages/PartyListPage';
 import Reset from './pages/ResetPage';
+import Select from './pages/SelectPage';
+import Ott from './pages/OttPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
+      <AnimatePresence>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
 
-        <Route path="/code=:code" element={<Reset />} />
-        <Route path="/signIn" element={<SignIn />} />
-        <Route path="/signUp" element={<SignUp />} />
+          <Route path="/code=:code" element={<Reset />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
 
           <Route path="/addParty" element={<AddParty />} />
 
@@ -32,6 +35,7 @@ function App() {
           {/* <Route path="/mypage/myparty" element={<MyParty />} />
             <Route path="/mypage/myparty/:id" element={<MyPartyDetail />} />
             <Route path="/mypage/myparty/:id/chat" element={<Chat />} />
+            <Route path="/select" element={<Select />} />
             */}
         </Routes>
       </AnimatePresence>
