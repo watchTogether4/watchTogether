9d969d1be19bcb66f38d9cd7cb9083ebbeb2a085
frontWeb-dev <frontend_web@kakao.com>
2022-11-03 16:23:35 +0900

    chore: 마이페이지 수정
    - Link 삭제, navigate 이용

--- client/src/components/Mypage/User.jsx
+++ client/src/components/Mypage/User.jsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useQuery } from 'react-query';
 import { IoIosArrowForward } from 'react-icons/io';
 import { InfoList, List } from './User.styles';
 import Profile from './Profile';
+import { getInfo } from './../../api/Users';
 
 function Chat() {
+  const getUserInfo = () => {
+    return getInfo().then((res) => res.data);
+  };
+  const navigate = useNavigate();
+  const { data, isLoading } = useQuery('getInfo', getUserInfo);
+
+  const handleClick = (path) => {
+    path && navigate(`./${path}`, { state: data });
+  };
   return (
     <>
       <Profile />
       <InfoList direction="column" alignItems="flex-start">
-        <List justifyContent="space-between">
-          <Link to="./user">
-            내 정보
-            <IoIosArrowForward />
-          </Link>
+        <List justifyContent="space-between" onClick={() => handleClick('user')}>
+          내 정보
+          <IoIosArrowForward />
         </List>
-        <List justifyContent="space-between">
-          <Link to="/">
-            내 파티
-            <IoIosArrowForward />
-          </Link>
+        <List justifyContent="space-between" onClick={() => handleClick()}>
+          내 파티
+          <IoIosArrowForward />
         </List>
-        <List justifyContent="space-between">
-          <Link to="/">
-            결제 내역
-            <IoIosArrowForward />
-          </Link>
+        <List justifyContent="space-between" onClick={() => handleClick()}>
+          결제 내역
+          <IoIosArrowForward />
         </List>
       </InfoList>
     </>
