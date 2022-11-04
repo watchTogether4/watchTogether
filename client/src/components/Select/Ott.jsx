9e2a0579a003855044e2af602bf1cee0a0bc5916
frontWeb-dev <frontend_web@kakao.com>
2022-11-05 04:27:50 +0900

    chore: Ott 페이지 수정
    - OTT 선택 시 월 이용료도 함께 보여주도록 변경

--- client/src/components/Select/Ott.jsx
+++ client/src/components/Select/Ott.jsx
@@ -1,53 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { Wrapper, OttButton, OttSection, ButtonSection } from './Ott.styles';
+import { Wrapper, OttButton, OttTitle, OttText, OttSection, ButtonSection } from './Ott.styles';
 import { Button } from '../../styles/Common';
 import { motion } from 'framer-motion';
+import otts from '../../mocks/platform';
 
 function Ott() {
   const navigate = useNavigate();
   const [ott, setOtt] = useState(-1);
-  const handleNetflix = ()=> {setOtt(1);};
-  const handleTving = ()=> {setOtt(2);};
-  const handleWatcha = ()=> {setOtt(3);};
-  const handleDisney = ()=> {setOtt(4);};
-  const handleWavve = ()=> {setOtt(5);};
-  const {state} = useLocation();
+
+  const handleClick = (id) => {
+    setOtt(id);
+  };
+
+  const { state } = useLocation();
   const role = state.role;
-  
-  return(
+
+  return (
     <motion.div
-      className='selectPage'
-      initial={{ opacity: 0}}
+      className="selectPage"
+      initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <Wrapper direction='column' justifyContent='flex-start'>
-        <OttSection direction='column' justifyContent='flex-start'>
-          <OttButton ott={ott} clicked={(ott === 1) ? true : false} onClick={()=> handleNetflix()}>
-            넷플릭스
-          </OttButton>
-          <OttButton ott={ott} clicked={(ott === 2) ? true : false} onClick={()=> handleTving()}>
-            티빙
-          </OttButton>
-          <OttButton ott={ott} clicked={(ott === 3) ? true : false} onClick={()=> handleWatcha()}>
-            왓챠
-          </OttButton>
-          <OttButton ott={ott} clicked={(ott === 4) ? true : false} onClick={()=> handleDisney()}>
-            디즈니플러스
-          </OttButton>
-          <OttButton ott={ott} clicked={(ott === 5) ? true : false} onClick={()=> handleWavve()}>
-            웨이브
-          </OttButton>
+      <Wrapper direction="column" justifyContent="flex-start">
+        <OttSection direction="column" justifyContent="flex-start">
+          {otts.map((platform) => (
+            <OttButton
+              direction="column"
+              ott={ott}
+              clicked={ott === platform.id ? true : false}
+              onClick={() => handleClick(platform.id)}
+            >
+              <OttTitle>{platform.name}</OttTitle>
+
+              {ott === platform.id && (
+                <div>
+                  <OttText>월 이용료 : {platform.price.toLocaleString('ko-KR')}원</OttText>
+                  <OttText>
+                    가치와치에서는 월
+                    {/* 월 이용료 / 4 금액에 파티장이면 500, 파티원이면 900원 추가한 금액으로 보여지게끔*/}
+                    <span>
+                      {' '}
+                      {(role ? platform.price / 4 + 500 : platform.price / 4 + 900).toLocaleString(
+                        'ko-KR',
+                      )}{' '}
+                    </span>
+                    원
+                  </OttText>
+                </div>
+              )}
+            </OttButton>
+          ))}
         </OttSection>
-        <ButtonSection direction='column' justifyContent='flex-start'>
-          <Button onClick={()=> navigate('/addParty', {state: {role: {role}, ott: {ott}}})}>
-              다음
+        <ButtonSection direction="column" justifyContent="flex-start">
+          <Button onClick={() => navigate('/addParty', { state: { role: role, ott: ott } })}>
+            다음
           </Button>
         </ButtonSection>
       </Wrapper>
     </motion.div>
   );
 }
-  
+
 export default Ott;
