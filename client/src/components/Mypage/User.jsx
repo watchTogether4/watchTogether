51a475e5187ceae929884f78e5fa3227ea518e64
frontWeb-dev <frontend_web@kakao.com>
2022-11-04 16:02:35 +0900

    feat: 마이 페이지 수정
    - 내 정보 링크 들어가기 전, 비밀번호 한번 더 확인하는 모달 추가

--- client/src/components/Mypage/User.jsx
+++ client/src/components/Mypage/User.jsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { IoIosArrowForward } from 'react-icons/io';
 import { InfoList, List } from './User.styles';
 import Profile from './Profile';
 import { getInfo } from './../../api/Users';
+import PasswordConfirm from './PasswordConfirm';
 
 function Chat() {
   const getUserInfo = () => {
     return getInfo().then((res) => res.data);
   };
-  const navigate = useNavigate();
   const { data, isLoading } = useQuery('getInfo', getUserInfo);
+
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = (path) => {
     path && navigate(`./${path}`, { state: data });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate('./user', { state: data });
+  };
+
   return (
     <>
       <Profile data={data} />
       <InfoList direction="column" alignItems="flex-start">
-        <List justifyContent="space-between" onClick={() => handleClick('user')}>
+        <List justifyContent="space-between" onClick={() => setIsOpen(true)}>
           내 정보
           <IoIosArrowForward />
         </List>
@@ -33,6 +42,8 @@ function Chat() {
           <IoIosArrowForward />
         </List>
       </InfoList>
+
+      {isOpen && <PasswordConfirm modal={setIsOpen} handleSubmit={handleSubmit} data={data} />}
     </>
   );
 }
