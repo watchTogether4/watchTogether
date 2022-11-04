c3cde4c14732e4c58cdc97f88c6cf47aa6b45fa8
frontWeb-dev <frontend_web@kakao.com>
2022-11-05 04:29:09 +0900

    chore: 파티 리스트 카드 스타일 수정

--- client/src/components/PartyList/Card.styles.jsx
+++ client/src/components/PartyList/Card.styles.jsx
@@ -2,45 +2,39 @@ import styled from 'styled-components';
 
 const CardWrapper = styled.div`
   width: 100%;
-  height: 25%;
+  margin-bottom: 1rem;
   display: flex;
   flex-direction: row;
   padding: 1rem 1.2rem;
   background-color: #fff;
-  border-radius: 12px;
+  border-radius: 1rem;
   align-items: center;
-  border-top: 1px solid lightgrey;
-  border-bottom: 1px solid lightgrey;
-`; 
+  border: 1px solid ${(props) => props.theme.color.light_300};
+`;
 
 const CardOtt = styled.img`
   height: 100%;
   width: 15%;
-  padding: 0.4rem;
+  margin-right: 1rem;
+`;
+
+const CardDesc = styled.div`
+  width: 100%;
+  padding-right: 1rem;
 `;
 
 const CardTitle = styled.p`
-  height: 100%;
-  font-size: 1.2rem;
+  margin-bottom: 0.2rem;
+  font-size: 1rem;
   font-weight: 600;
   line-height: 1.2rem;
-  padding: 1rem 1.4rem;
-  width: 70%;
 `;
 
 const CardPerson = styled.p`
-  height: 100%;
-  font-size: 1.2rem;
+  font-size: 0.85rem;
   font-weight: 400;
   line-height: 1.2rem;
-  padding: 1rem;
   color: grey;
-  width: 15%;
 `;
 
-export {
-    CardWrapper,
-    CardOtt,
-    CardTitle,
-    CardPerson,
-};
+export { CardWrapper, CardOtt, CardDesc, CardTitle, CardPerson };
