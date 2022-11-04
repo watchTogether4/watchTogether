82a94a72a6155f0e39dcc777697a6e16bd69b87f
frontWeb-dev <frontend_web@kakao.com>
2022-11-05 04:28:36 +0900

    refactor: 파티 리스트 코드 리팩토링

--- client/src/components/PartyList/PartyList.jsx
+++ client/src/components/PartyList/PartyList.jsx
@@ -1,32 +1,38 @@
-import React, { useState } from 'react';
-import { Card } from './Card'
+import React from 'react';
+import { useQuery } from 'react-query';
+import { Card } from './Card';
 import { showPartyList } from './../../api/Parties';
 import { Wrapper, Board } from './PartyList.styles';
-import icons from '../../mocks/platform';
+import platform from '../../mocks/platform';
 
 function PartyList() {
   const accessToken = localStorage.getItem('access-token');
-  const [boardList, setBoardList] = useState([]);
+
   const getBoardList = () => {
-    return showPartyList(accessToken).then((res) => {
-      console.log(res.data)});
+    return showPartyList(accessToken).then((res) => res.data);
   };
-  getBoardList().then(result=> setBoardList(result));
-  // const sortList = () => {
-  // };
+
+  // 파티 리스트가 [{}, {}] 형태로 data에 쌓임
+  const { data } = useQuery('getBoardList', getBoardList);
 
   return (
     <>
       <Wrapper>
-        <Board>
-          {boardList.map((data) => (
-            <Card 
-              ottUrl={data.partyOttId} 
-              title={data.title} 
-              people={data.people}
-            />
-          ))}
-        </Board>
+        {/* data가 있을 경우에만 렌더링  */}
+        {data && (
+          <Board>
+            {data.map((party) => (
+              <Card
+                // map 사용 시 key 값 필수 -> 유니크한 값 (보통 index나 id 사용 )
+                key={party.id}
+                // platform 중에 ottId 가 같은 것만 모아서 새 배열로 리턴
+                ottUrl={party.ottId ? platform.filter((a) => a.id === party.ottId) : ''}
+                title={party.title}
+                people={party.people}
+              />
+            ))}
+          </Board>
+        )}
       </Wrapper>
     </>
   );
