61f81a13eaeab2f3b1794a8cedd46d15d1a44e80
kdjdj106 <kdjdj106@naver.com>
2022-11-08 17:26:20 +0900

    fix : 메세지 세분화 및 파티 나가기 기능 수정

--- server/src/main/java/com/watchtogether/server/party/service/Application/CheckPartyApplication.java
+++ server/src/main/java/com/watchtogether/server/party/service/Application/CheckPartyApplication.java
@@ -1,22 +1,32 @@
 package com.watchtogether.server.party.service.Application;
 
+
+import com.watchtogether.server.alert.service.AlertService;
 import com.watchtogether.server.exception.PartyException;
 import com.watchtogether.server.exception.type.PartyErrorCode;
+import com.watchtogether.server.ott.domain.dto.OttDto;
+import com.watchtogether.server.ott.service.OttService;
 import com.watchtogether.server.party.domain.entitiy.Party;
 import com.watchtogether.server.party.domain.entitiy.PartyMember;
 import com.watchtogether.server.party.domain.model.LeavePartyForm;
+import com.watchtogether.server.party.domain.model.LeavePartyResponseForm;
 import com.watchtogether.server.party.domain.repository.PartyMemberRepository;
 import com.watchtogether.server.party.domain.repository.PartyRepository;
+import com.watchtogether.server.party.domain.type.AlertType;
 import com.watchtogether.server.party.service.impl.PartyServiceImpl;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import com.watchtogether.server.users.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.watchtogether.server.party.domain.type.AlertType.*;
 
 @Service
 @Slf4j
@@ -26,9 +36,10 @@ public class CheckPartyApplication {
         private final PartyMemberRepository partyMemberRepository;
         private final PartyServiceImpl partyService;
 
-        // todo 결제 부분
-//        private final OttService ottService;
-//        private final TransactionService transactionService;
+        private final OttService ottService;
+        private final TransactionService transactionService;
+
+        private final AlertService alertService;
 
 
 
@@ -54,16 +65,14 @@ public class CheckPartyApplication {
 
         } else {
             for (Party party : partyList) {
-                // 결제 진행부분 삽입
-                // todo 결제 부분
-//                OttDto ottDto = ottService.searchOtt(party.getOttId());
-//
-//                transactionService.userCashDeposit(
-//                    party.getMembers(),
-//                    party.getLeaderNickname(),
-//                    party.getId(),
-//                    ottDto.getCommissionLeader(),
-//                    ottDto.getFee());
+                OttDto ottDto = ottService.searchOtt(party.getOttId());
+
+                transactionService.userCashDeposit(
+                    party.getMembers(),
+                    party.getLeaderNickname(),
+                    party.getId(),
+                    ottDto.getCommissionLeader(),
+                    ottDto.getFee());
 
 
                 party.setPayDt(party.getPayDt().plusMonths(1));
@@ -71,6 +80,9 @@ public class CheckPartyApplication {
                 log.info("오늘 결제한 파티는" + party.getId() + "번 " + party.getTitle() + "입니다");
 
                 // 결제 진행되었다고 메시지 전송
+                sendAlert(party, TRANSACTION);
+
+
 
             }
         }
@@ -95,7 +107,7 @@ public class CheckPartyApplication {
                         partyMember.get().setAlertCheck(false);
                         partyMemberRepository.save(partyMember.get());
                         // 파티 갱신할껀지 말지 메시지 전송
-
+                        sendAlert(party, CONTINUE);
                     }
                 }
             }
@@ -111,21 +123,54 @@ public class CheckPartyApplication {
             for (Party party : partyList) {
                 for (int i = 0; i < party.getMembers().size(); i++) {
                     String nickname = party.getMembers().get(i).getNickName();
-                    Optional<PartyMember> partyMember = partyMemberRepository.findByNickNameAndParty(nickname, party);
-                    if (partyMember.isEmpty()) {
-                        throw new PartyException(PartyErrorCode.NOT_FOUND_USER);
-                    }else {
-                        if (!partyMember.get().isAlertCheck()){
+                    PartyMember partyMember = partyMemberRepository.findByNickNameAndParty(nickname, party)
+                            .orElseThrow(()-> new PartyException(PartyErrorCode.NOT_FOUND_USER));
+                        if (!partyMember.isAlertCheck()){
                             LeavePartyForm leavePartyForm = LeavePartyForm.builder()
                                     .partyId(party.getId())
                                     .nickName(nickname)
                                     .build();
-                            partyService.leaveParty(leavePartyForm);
+                            LeavePartyResponseForm form = partyService.IgnoreContinueMessage(leavePartyForm);
+                            if (form.isLeader()){
+                                // TODO: 2022-11-08 파티장이 탈퇴하여 삭제된다는 메세지 발송 및 파티 삭제
+                                sendAlert(party, LEAVE_LEADER);
+                                partyService.deleteParty(party.getId());
+                            }else {
+                                sendAlert(party, LEAVE_MEMBER);
+                            }
                         }
-                    }
+
                 }
             }
         }
     }
+
+    private void sendAlert(Party party, AlertType type) {
+        List<PartyMember> partyMemberList = party.getMembers();
+        for (int i = 0; i < party.getMembers().size(); i++) {
+            List<String> nickname = new ArrayList<>();
+            nickname.add(partyMemberList.get(i).getNickName());
+            String message = "";
+            switch (type){
+                case INVITE: message= "초대메세지";
+                    break;
+                case LEAVE_LEADER:message= "리더가 떠났다는 메세지 및 파티 삭제 메세지";
+                    break;
+                case LEAVE_MEMBER:message= "파티원이 떠났다는 메세지";
+                    break;
+                case CHANGE_PASSWORD:message= " 비밀번호 변경 메세지";
+                    break;
+                case CONTINUE: message= "파티 갱신 메세지";
+                    break;
+                case TRANSACTION:message= "거래 발생 메세지";
+                    break;
+
+            }
+            // todo 메시지 형식 작성되면 그 형식에 따라서 메시지 작성
+            alertService.createAlert(nickname, party.getId(), null, message, type);
+
+        }
+    }
+    // TODO: 2022-11-08 파티객체 삭제 메서드 구현 필요!! (파티원을 다 채우지 못한경우)
 
 }
