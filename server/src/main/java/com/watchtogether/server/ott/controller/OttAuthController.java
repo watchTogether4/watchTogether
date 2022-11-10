d3b2a280eadaafd5b0184d5b23d0a93171fec84d
dohoonkim <dohoonkim023@gmail.com>
2022-11-10 11:17:34 +0900

    chore: swagger를 사용하여 API 명세서 작성

--- server/src/main/java/com/watchtogether/server/ott/controller/OttAuthController.java
+++ server/src/main/java/com/watchtogether/server/ott/controller/OttAuthController.java
@@ -3,6 +3,7 @@ package com.watchtogether.server.ott.controller;
 import com.watchtogether.server.ott.domain.dto.LoginResult;
 import com.watchtogether.server.ott.domain.dto.OttAuthRequest;
 import com.watchtogether.server.ott.service.OttAuthService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "OTT인중(OttAuthController)", description = "OttAuth API")
 @RequestMapping("/api/v1/ottAuth")
-
 public class OttAuthController {
 
     private final OttAuthService partyService;
