dadde6504aca8e17309ca246b487f345361c1cd8
frontweb-dev <frontend_web@kakao.com>
2023-04-19 12:42:38 +0900

    refactor: mocks & api 타입 스크립트 적용

--- client/src/api/Token.ts
+++ client/src/api/Token.ts
@@ -9,8 +9,8 @@ API.interceptors.request.use((res) => {
   const accessToken = getAccessToken();
 
   if (accessToken) {
-    res.headers['Content-Type'] = 'application/json;charset=UTF-8';
-    res.headers['Authorization'] = `Bearer ${accessToken}`;
+    res.headers!['Content-Type'] = 'application/json;charset=UTF-8';
+    res.headers!['Authorization'] = `Bearer ${accessToken}`;
   }
   return res;
 });
--- client/src/mocks/platform.ts
+++ client/src/mocks/platform.ts
@@ -10,7 +10,7 @@ const icons = [
   {
     id: 2,
     name: '티빙',
-    type:'TVING',
+    type: 'TVING',
     image:
       'https://user-images.githubusercontent.com/101559564/196022008-cc4cc62d-7e02-4f0a-b22a-f1173b130f22.png',
     price: 13900,
@@ -18,7 +18,7 @@ const icons = [
   {
     id: 3,
     name: '웨이브',
-    type:'WAVVE',
+    type: 'WAVVE',
     image:
       'https://user-images.githubusercontent.com/101559564/196022012-e18eb05e-c79b-4666-b3b5-730cae708be6.png',
     price: 13900,
@@ -26,7 +26,7 @@ const icons = [
   {
     id: 4,
     name: '디즈니플러스',
-    type:'DISNEY',
+    type: 'DISNEY',
     image:
       'https://user-images.githubusercontent.com/101559564/196022010-635447f6-e456-4f20-a701-3f33ce2ae5d8.png',
     price: 9900,
@@ -34,7 +34,7 @@ const icons = [
   {
     id: 5,
     name: '왓챠',
-    type:'WATCHA',
+    type: 'WATCHA',
     image:
       'https://user-images.githubusercontent.com/101559564/196022016-2d77cc4d-8dbd-449e-84ff-46f4614ae779.png',
     price: 12900,
